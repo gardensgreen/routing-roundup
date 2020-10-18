e040e7f813269ca4091d7b64bf67767d7f92c9fb
Daniel TIllero <danieltillero67@gmail.com>
2020-10-17 21:08:50 -0400

    Define xyz route

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 const express = require("express");
 
 const app = express();
-app.set('view engine', 'pug');
+app.set("view engine", "pug");
 
-
-// app.all('*', (req, res) => {
-//     const pageData = { 
-//         path: req.path, 
-//         method: req.method, 
-//         randomNumber: Math.floor(Math.random() * 100) 
-//     }
-//     res.render('layout', pageData)
-// })
+// app.all("*", (req, res) => {
+//     const pageData = {
+//         path: req.path,
+//         method: req.method,
+//         randomNumber: Math.floor(Math.random() * 100),
+//     };
+//     res.render("layout", pageData);
+// });
 
 app.get("/", (req, res) => {
     res.send("Hello from Express!");
+});
+
+app.get("/*xyz", (req, res) => {
+    res.send("That's all I wrote.");
 });
 
 const port = 8081;
