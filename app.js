2abe3738acaedbd0a2069050f610c3c4528874e3
Daniel TIllero <danieltillero67@gmail.com>
2020-10-17 23:17:00 -0400

    Defining a collection of route handlers

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const express = require("express");
+const router = require("./routes");
 
 const app = express();
 app.set("view engine", "pug");
-
-
 
 app.get("/", (req, res) => {
     res.send("Hello from Express!");
@@ -13,10 +12,12 @@ app.get("/*xyz", (req, res) => {
     res.send("That's all I wrote.");
 });
 
-app.get('/capital-letters/:path', (req, res) => {
+app.get("/capital-letters/:path", (req, res) => {
     let path = req.params.path;
-    res.send((path).toUpperCase());
-})
+    res.send(path.toUpperCase());
+});
+
+app.use(["/margot", "/margeaux"], router);
 
 /*  app.get(/^\/(capital-letters)\/(.*)/, (req, res) => {
     let path = req.params[1];
