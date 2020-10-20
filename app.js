73e0d5348050ae427924bb318032507f710e9277
maxwelljmckee <maxwelljmckee@gmail.com>
2020-10-20 14:06:46 -0600

    we r so smart and cool

--- app.js
+++ app.js
@@ -3,14 +3,7 @@ const express = require("express");
 const app = express();
 app.set("view engine", "pug");
 
-// app.all("*", (req, res) => {
-//     const pageData = {
-//         path: req.path,
-//         method: req.method,
-//         randomNumber: Math.floor(Math.random() * 100),
-//     };
-//     res.render("layout", pageData);
-// });
+
 
 app.get("/", (req, res) => {
     res.send("Hello from Express!");
@@ -18,6 +11,25 @@ app.get("/", (req, res) => {
 
 app.get("/*xyz", (req, res) => {
     res.send("That's all I wrote.");
+});
+
+app.get('/capital-letters/:path', (req, res) => {
+    let path = req.params.path;
+    res.send((path).toUpperCase());
+})
+
+/*  app.get(/^\/(capital-letters)\/(.*)/, (req, res) => {
+    let path = req.params[1];
+    res.send(path.toUpperCase());
+})  */
+
+app.all("*", (req, res) => {
+    const pageData = {
+        path: req.path,
+        method: req.method,
+        randomNumber: Math.floor(Math.random() * 100),
+    };
+    res.render("layout", pageData);
 });
 
 const port = 8081;
