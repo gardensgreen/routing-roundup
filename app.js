31740d9e30d47c83005324be8ba93561e9c55d19
maxwelljmckee <maxwelljmckee@gmail.com>
2020-10-20 12:04:51 -0600

    phase 2 complete

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require("express");
 
 const app = express();
+app.set('view engine', 'pug');
+
+
+// app.all('*', (req, res) => {
+//     const pageData = { 
+//         path: req.path, 
+//         method: req.method, 
+//         randomNumber: Math.floor(Math.random() * 100) 
+//     }
+//     res.render('layout', pageData)
+// })
 
 app.get("/", (req, res) => {
     res.send("Hello from Express!");
 });
 
-const port = 8080;
+const port = 8081;
 app.listen(port, () => console.log("Listening on port " + port));
